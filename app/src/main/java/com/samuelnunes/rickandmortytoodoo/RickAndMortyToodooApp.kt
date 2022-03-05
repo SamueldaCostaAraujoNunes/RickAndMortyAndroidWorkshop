35bf03582091015aae1851e3f6745faf48780bbe
Samuel Nunes <samueldacostaaraujonunes@gmail.com>
2022-03-04 23:21:41 -0300

    Setup dagger lib

---
+++ app/src/main/java/com/samuelnunes/rickandmortytoodoo/RickAndMortyToodooApp.kt
@@ -0,0 +1,7 @@
+package com.samuelnunes.rickandmortytoodoo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RickAndMortyToodooApp : Application()
